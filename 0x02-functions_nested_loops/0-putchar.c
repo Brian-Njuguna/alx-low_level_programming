c05ebaf66aa1f223606b391ef5991c43ffe31966
Brian Njuguna <briannjugush52@gmail.com>
2023-10-14 08:29:49 -0700

    correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+/*
+ * File: 0-putchar.c
+ * Auth: Brian
+ *
+ */
+
 #include "main.h"
 
+/**
+ * main -Prints "_putchar" followed by a new line.
+ *
+ * Return:Always 0.
+ */
 int main(void)
 {
 	_putchar('_');
