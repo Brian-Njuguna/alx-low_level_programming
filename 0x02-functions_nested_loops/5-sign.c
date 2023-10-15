eced5829ed3fad9a16754e3bc00dce669604d4c4
Brian Njuguna <briannjugush52@gmail.com>
2023-10-15 00:33:40 -0700

    betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign -prints the sign of a number
+ * print_sign - prints the sign of a number
  *
  */
 
 int print_sign(int n)
 {
-	if (n >0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
