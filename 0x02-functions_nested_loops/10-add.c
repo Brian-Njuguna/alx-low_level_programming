22e900e13fcde5214aa6347e725faef68fd4bfa6
Brian Njuguna <briannjugush52@gmail.com>
2023-10-15 02:10:10 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,8 +4,9 @@
  * add -sum of two integers
  * @num1: first interger
  * @num2: second interger
+ * Return: Result after summing up
  */
-int add (int num1, int num2)
+int add(int num1, int num2)
 {
-	return (num1 +num2);
+	return (num1 + num2);
 }
