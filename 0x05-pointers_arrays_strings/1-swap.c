1413bec5f7dea61027035bdb538da1970a04a5c7
Brian Njuguna <briannjugush52@gmail.com>
2023-10-22 01:13:10 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,13 +6,20 @@
  * @a: pointer a
  * @b: pointer b
  */
-void swap_int(int *a, int *b)
-{
-	int c;
+void swap_int(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-	c = *a;
-	*a = *b
-	*b = c;
+int main() {
+	int num1 =5;
+	int num2 = 10;
+
+	printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+	swap_int(&num1, &num2);
+	printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+	return 0;
 }
 	
 
